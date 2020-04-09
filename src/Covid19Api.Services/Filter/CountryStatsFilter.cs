94b2d947d16435698b6041d700faf9dc628d2f13
alsami <al.khatib.sami1990@gmail.com>
2020-04-09 18:35:06 +0200

    fix: exclude regions

--- src/Covid19Api.Services/Filter/CountryStatsFilter.cs
+++ src/Covid19Api.Services/Filter/CountryStatsFilter.cs
@@ -5,9 +5,10 @@ namespace Covid19Api.Services.Filter
 {
     public static class CountryStatsFilter
     {
-        public static Lazy<Func<CountryStats, bool>> ValidOnly = new Lazy<Func<CountryStats, bool>>(stats =>
+        public static readonly Lazy<Func<CountryStats, bool>> ValidOnly = new Lazy<Func<CountryStats, bool>>(stats =>
             !string.IsNullOrWhiteSpace(stats.Country) &&
             stats.Country != "World" &&
+            !stats.Country.StartsWith("<nobr>") &&
             !stats.Empty());
     }
 }
