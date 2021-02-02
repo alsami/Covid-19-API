9cd94f02cc11d6bdee87dff688e5f4534dc6fbad
alsami <al.khatib.sami1990@gmail.com>
2021-02-02 10:40:42 +0100

    fix: make sure cache-key for calculating vary of specific countries is set unique

--- src/Covid19Api.UseCases.Abstractions/Queries/CountryStatistics/CalculateVaryForCountryStatisticsQuery.cs
+++ src/Covid19Api.UseCases.Abstractions/Queries/CountryStatistics/CalculateVaryForCountryStatisticsQuery.cs
@@ -9,6 +9,6 @@ namespace Covid19Api.UseCases.Abstractions.Queries.CountryStatistics
 {
     public record CalculateVaryForCountryStatisticsQuery(string Country, DateTime MinFetchedAt) : ICacheableRequest, IRequest<IEnumerable<CountryVaryStatisticContainerDto>>
     {
-        public CacheConfiguration GetCacheConfiguration() => new(nameof(CalculateVaryForCountryStatisticsQuery), TimeSpan.FromMinutes(30));
+        public CacheConfiguration GetCacheConfiguration() => new($"{this.Country}_{this.MinFetchedAt.Date:dd.MM.yyyy}_{nameof(CalculateVaryForCountryStatisticsQuery)}", TimeSpan.FromMinutes(30));
     }
 }
